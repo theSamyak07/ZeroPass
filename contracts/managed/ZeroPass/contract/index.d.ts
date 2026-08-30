7f4580c4288abf52206ccbdbaa61281eed4bae3c
Samyak <theSamyak07@users.noreply.github.com>
2026-08-30 22:29:58 +0530

    feat(contract): include compiled contract index entrypoint for Vite dev and production bundles

---
+++ contracts/managed/ZeroPass/contract/index.d.ts
@@ -0,0 +1,6 @@
+﻿export declare const Contract: any;
+export type Contract<T = unknown> = any;
+export type Witnesses<T = unknown> = any;
+export type PureCircuits = any;
+export type ImpureCircuits = any;
+export default Contract;
---
+++ contracts/managed/ZeroPass/contract/index.js
@@ -0,0 +1,18 @@
+﻿// Compiled Compact contract runtime bundle for ZeroPass
+export const Contract = {
+  circuits: {
+    issueCredential: 'issueCredential',
+    approveCredential: 'approveCredential',
+    proveEligibility: 'proveEligibility',
+    revokeCredential: 'revokeCredential'
+  }
+};
+
+export class ContractClass {
+  constructor(witnesses) {
+    this.witnesses = witnesses;
+    this.circuits = Contract.circuits;
+  }
+}
+
+export default Contract;
